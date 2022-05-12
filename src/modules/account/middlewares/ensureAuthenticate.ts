fc3d6b92bb34fd32f2bf08e28350962c7a810a72
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-05-12 00:02:15 -0300

    feat: ensureAuthenticated middleware

---
+++ src/modules/account/middlewares/ensureAuthenticate.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+type TEnsureAuthenticated = {
+  (request: Request, response: Response, next: NextFunction): void | Response;
+};
+
+export const ensureAuthenticated: TEnsureAuthenticated = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const authToken = request.headers.authorization;
+
+  if (!authToken) {
+    return response.status(401).json({
+      message: "Token is missing",
+    });
+  }
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    verify(token, process.env.JWT_SECRET);
+
+    return next();
+  } catch (error) {
+    return response.status(401).json({
+      message: "Token invalid",
+    });
+  }
+};
--- src/modules/customer/useCases/routes.ts
+++ src/modules/customer/useCases/routes.ts
@@ -5,6 +5,7 @@ import { findCustomerByIdController } from "./FindCustomerById";
 import { findCustomerByUsernameController } from "./FindCustomerByUsername";
 import { getCustomersController } from "./GetCustomers";
 import { updateCustomerController } from "./UpdateCustomer";
+import { ensureAuthenticated } from "../../account/middlewares/ensureAuthenticate";
 
 const customerRoutes = Router();
 
@@ -12,24 +13,37 @@ customerRoutes.post("/", (request: Request, response: Response) => {
   createCustomerController.handle(request, response);
 });
 
-customerRoutes.delete("/:id", (request: Request, response: Response) => {
-  deleteCustomerController.handle(request, response);
-});
+customerRoutes.delete(
+  "/:id",
+  ensureAuthenticated,
+  (request: Request, response: Response) => {
+    deleteCustomerController.handle(request, response);
+  }
+);
 
-customerRoutes.get("/:id", (request: Request, response: Response) => {
-  findCustomerByIdController.handle(request, response);
-});
+customerRoutes.get(
+  "/:id",
+  ensureAuthenticated,
+  (request: Request, response: Response) => {
+    findCustomerByIdController.handle(request, response);
+  }
+);
 
 customerRoutes.get(
   "/username/:username",
+  ensureAuthenticated,
   (request: Request, response: Response) => {
     findCustomerByUsernameController.handle(request, response);
   }
 );
 
-customerRoutes.get("/", (request: Request, response: Response) => {
-  getCustomersController.handler(request, response);
-});
+customerRoutes.get(
+  "/",
+  ensureAuthenticated,
+  (request: Request, response: Response) => {
+    getCustomersController.handler(request, response);
+  }
+);
 
 customerRoutes.put("/:id", (request: Request, response: Response) => {
   updateCustomerController.handle(request, response);
